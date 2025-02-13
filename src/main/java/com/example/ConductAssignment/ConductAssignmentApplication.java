15822770ed0801895b52cd0264af49a057696745
Omkar Kadam <omkar.kadam@smarsh.com>
2025-02-13 13:00:27 +0530

    Initial repo

---
+++ src/main/java/com/example/ConductAssignment/ConductAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConductAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConductAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConductAssignment/ConductAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConductAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConductAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
